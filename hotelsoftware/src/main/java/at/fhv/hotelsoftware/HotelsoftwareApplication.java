2e3c86e2418000493dadb3254668f4849e82d713
Adrian <adrian.essig@outlook.com>
2021-10-25 00:33:28 +0200

    first commit

---
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/HotelsoftwareApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.hotelsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsoftwareApplication.class, args);
+	}
+
+}
---
+++ hotelsoftware/src/test/java/at/fhv/hotelsoftware/HotelsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.hotelsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
