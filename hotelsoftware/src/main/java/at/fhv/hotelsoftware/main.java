3155e1382c6a989cd1cc75155be00509dc1152d5
ShortyIT <tobias.kurz@students.fhv.at>
2021-10-26 18:49:15 +0200

    h2 database added - still can't login

---
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/main.java
@@ -0,0 +1,9 @@
+package at.fhv.hotelsoftware;
+
+import org.springframework.boot.SpringApplication;
+
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsoftwareApplication.class, args);
+    }
+}
