feab0e32c94912a63d918618b55dbc08c1b3169e
Fabian_Egartner <fabian.egartner@hotmail.com>
2021-11-04 19:17:23 +0100

    Form Data Transfer Completed

---
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Customer.java
@@ -0,0 +1,30 @@
+package at.fhv.hotelsoftware.domain.model;
+
+public class Customer {
+    private String fname;
+    private String lname;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                '}';
+    }
+}
---
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Dummy.java
@@ -0,0 +1,113 @@
+package at.fhv.hotelsoftware.domain.model;
+
+public class Dummy {
+
+    private String fname;
+    private String lname;
+    private String voucherCode;
+    private String streetAdr;
+    private String zip;
+    private String city;
+    private String country;
+    private String phoneNumber;
+    private String email;
+    private String singleRoomCount;
+    private String doubleRoomCount;
+    private String luxusRoomCount;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getVoucherCode() {
+        return voucherCode;
+    }
+
+    public void setVoucherCode(String voucherCode) {
+        this.voucherCode = voucherCode;
+    }
+
+    public String getStreetAdr() {
+        return streetAdr;
+    }
+
+    public void setStreetAdr(String streetAdr) {
+        this.streetAdr = streetAdr;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSingleRoomCount() {
+        return singleRoomCount;
+    }
+
+    public void setSingleRoomCount(String singleRoomCount) {
+        this.singleRoomCount = singleRoomCount;
+    }
+
+    public String getDoubleRoomCount() {
+        return doubleRoomCount;
+    }
+
+    public void setDoubleRoomCount(String doubleRoomCount) {
+        this.doubleRoomCount = doubleRoomCount;
+    }
+
+    public String getLuxusRoomCount() {
+        return luxusRoomCount;
+    }
+
+    public void setLuxusRoomCount(String luxusRoomCount) {
+        this.luxusRoomCount = luxusRoomCount;
+    }
+}
