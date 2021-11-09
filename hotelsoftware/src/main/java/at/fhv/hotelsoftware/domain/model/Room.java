ec29ba310774a005c0e858215a577d113d20f399
Adrian <Adrian.Essig@outlook.com>
2021-11-09 15:21:40 +0100

    basic xml

--- hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Room.java
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Room.java
@@ -2,6 +2,15 @@ package at.fhv.hotelsoftware.domain.model;
 
 public class Room {
     private int roomNumber;
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Room(int roomNumber) {
         this.roomNumber = roomNumber;
