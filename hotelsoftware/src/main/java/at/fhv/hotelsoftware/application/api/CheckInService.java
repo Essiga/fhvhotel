078c57ed357f478ec097428ed9e6606a910e9b07
Adrian <Adrian.Essig@outlook.com>
2021-11-12 13:43:01 +0100

    merged with dev

--- hotelsoftware/src/main/java/at/fhv/hotelsoftware/application/api/CheckInService.java
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/application/api/CheckInService.java
@@ -3,5 +3,6 @@ package at.fhv.hotelsoftware.application.api;
 import at.fhv.hotelsoftware.domain.model.BookingId;
 
 public interface CheckInService {
+    //
     void ChangeBookingStatus(BookingId bookingId);
 }
