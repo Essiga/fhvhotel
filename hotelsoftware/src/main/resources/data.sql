0f713c6fdafe032f017cbce3fb987ee2419d2d26
ShortyIT <tobias.kurz@students.fhv.at>
2021-11-16 15:03:35 +0100

    sql and rooms updated

---
+++ hotelsoftware/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into TBL_BOOKING
+values(NULL,1, 'Tobias Kurz', '2021-11-15', '2021-11-20', '2021-11-03', 'Single',1,0,0, 'CONFIRMED', NULL);
+
+insert into TBL_ROOM
+values(NULL,'SINGLE',101,1,null),
+(NULL,'SINGLE',102,1,null),
+(NULL,'DOUBLE',201,null,null),
+(NULL,'DOUBLE',202,null,null),
+(NULL,'LUXUS',301,null,null);
+
+
