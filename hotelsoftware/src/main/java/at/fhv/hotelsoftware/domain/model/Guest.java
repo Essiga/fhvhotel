5432a413c2b4128669a9ac128d756029ce60b980
fabian_egartner <fabian.egartner@hotmail.com>
2021-11-17 19:46:09 +0100

    - Guest created - GuestType created - GuestId created

--- hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Guest.java
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/model/Guest.java
@@ -1,10 +1,5 @@
 package at.fhv.hotelsoftware.domain.model;
 
-import java.time.LocalDate;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
-
 public class Guest {
 
     private Long id;
