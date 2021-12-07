2ca6fce8c9c785d25c92b0a20032eef8f6725c10
ShortyIT <tobias.kurz@students.fhv.at>
2021-12-07 15:16:48 +0100

    findAllGuests TDD

--- hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/api/GuestRepository.java
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/domain/api/GuestRepository.java
@@ -4,10 +4,12 @@ import at.fhv.hotelsoftware.domain.model.Guest;
 import at.fhv.hotelsoftware.domain.model.valueobjects.GuestId;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GuestRepository {
 
     void addGuest(Guest guest);
     Optional<Guest> findGuestById(GuestId guestId);
+    List<Guest> findAllGuest();
 }
--- hotelsoftware/src/test/java/at/fhv/hotelsoftware/infrastructure/GuestRepositoryTests.java
+++ hotelsoftware/src/test/java/at/fhv/hotelsoftware/infrastructure/GuestRepositoryTests.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -53,5 +55,37 @@ public class GuestRepositoryTests {
         assertEquals(idExpected.getGuestId(), guestActual.get().getGuestId().getGuestId());
         assertEquals(firstNameExpected, guestActual.get().getFirstName());
         assertEquals(lastNameExpected, guestActual.get().getLastName());
+    }
+
+    @Test
+    void given_guests_when_persistedflushedfetched_then_expectallguests() {
+        //given
+        GuestId idExpected = new GuestId(UUID.randomUUID());
+        String firstNameExpected = "Fabian";
+        String lastNameExpected = "Egartner";
+
+        Guest guestExpected = new Guest().builder().
+                guestId(idExpected).
+                firstName(firstNameExpected).
+                lastName(lastNameExpected).
+                street("Jahngasse 1").
+                zip("6850").
+                city("Dornbirn").
+                country("Austria").
+                phoneNumber("066023874").
+                email("abc@test.de").
+                build();
+
+
+        //when
+        guestRepository.addGuest(guestExpected);
+        em.flush();
+
+        List<Guest> allGuests = guestRepository.findAllGuest();
+
+        //then
+            assertEquals(idExpected.getGuestId(), allGuests.get(0).getGuestId().getGuestId());
+            assertEquals(firstNameExpected, allGuests.get(0).getFirstName());
+            assertEquals(lastNameExpected, allGuests.get(0).getLastName());
     }
 }
