67c2eb4c2479fac52dd48bcc2bb0e24910443bef
ShortyIT <tobias.kurz@students.fhv.at>
2021-12-08 17:29:41 +0100

    filter with javascript

---
+++ hotelsoftware/src/main/resources/static/filter.js
@@ -0,0 +1,24 @@
+function filterBookings(action) {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = action;
+    table = document.getElementById("bookings");
+    tr = table.getElementsByTagName("tr");
+
+    console.log(input, filter,table,tr);
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
