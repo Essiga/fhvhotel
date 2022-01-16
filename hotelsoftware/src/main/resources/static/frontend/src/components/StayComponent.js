bd66f34a86e642058ef1d74c57044f43efea653f
Fabian Egartner <fabian.egartner@hotmail.com>
2022-01-16 13:46:18 +0100

    - redesign spa

--- hotelsoftware/src/main/resources/static/frontend/src/components/StayComponent.js
+++ hotelsoftware/src/main/resources/static/frontend/src/components/StayComponent.js
@@ -90,7 +90,7 @@ class StayComponent extends React.Component {
     render() {
 
         return (
-            <div className="p-16 bg-gray-50 h-full w-full bg-no-repeat bg-cover opacity-80" style={{backgroundImage: `url(${vacation})`}}>
+            <div className="overflow-scroll p-16 bg-gray-50 h-full w-full bg-no-repeat bg-cover opacity-80" style={{backgroundImage: `url(${vacation})`}}>
 
                 <div className="py-4 h-1/12 mb-10 border-4 border-blue-200 rounded bg-gray-100 opacity-90">
                     <div className="w-full px-4 mb-8">
