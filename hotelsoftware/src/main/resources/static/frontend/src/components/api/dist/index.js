ae1a385be3abfede746fef9e7b134b34f89ebfa3
fabian_egartner <fabian.egartner@hotmail.com>
2022-01-21 19:42:52 +0100

    Integration Test fixed (failed because there are by default no rooms in the database)

---
+++ hotelsoftware/src/main/resources/static/frontend/src/components/api/dist/index.js
@@ -0,0 +1,79 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ApiClient", {
+  enumerable: true,
+  get: function get() {
+    return _ApiClient.default;
+  }
+});
+Object.defineProperty(exports, "BookingDataDTO", {
+  enumerable: true,
+  get: function get() {
+    return _BookingDataDTO.default;
+  }
+});
+Object.defineProperty(exports, "BookingForm", {
+  enumerable: true,
+  get: function get() {
+    return _BookingForm.default;
+  }
+});
+Object.defineProperty(exports, "BookingId", {
+  enumerable: true,
+  get: function get() {
+    return _BookingId.default;
+  }
+});
+Object.defineProperty(exports, "BookingRestControllerApi", {
+  enumerable: true,
+  get: function get() {
+    return _BookingRestControllerApi.default;
+  }
+});
+Object.defineProperty(exports, "GuestDTO", {
+  enumerable: true,
+  get: function get() {
+    return _GuestDTO.default;
+  }
+});
+Object.defineProperty(exports, "GuestId", {
+  enumerable: true,
+  get: function get() {
+    return _GuestId.default;
+  }
+});
+Object.defineProperty(exports, "RoomDTO", {
+  enumerable: true,
+  get: function get() {
+    return _RoomDTO.default;
+  }
+});
+Object.defineProperty(exports, "RoomPriceDTO", {
+  enumerable: true,
+  get: function get() {
+    return _RoomPriceDTO.default;
+  }
+});
+
+var _ApiClient = _interopRequireDefault(require("./ApiClient"));
+
+var _BookingDataDTO = _interopRequireDefault(require("./model/BookingDataDTO"));
+
+var _BookingForm = _interopRequireDefault(require("./model/BookingForm"));
+
+var _BookingId = _interopRequireDefault(require("./model/BookingId"));
+
+var _GuestDTO = _interopRequireDefault(require("./model/GuestDTO"));
+
+var _GuestId = _interopRequireDefault(require("./model/GuestId"));
+
+var _RoomDTO = _interopRequireDefault(require("./model/RoomDTO"));
+
+var _RoomPriceDTO = _interopRequireDefault(require("./model/RoomPriceDTO"));
+
+var _BookingRestControllerApi = _interopRequireDefault(require("./api/BookingRestControllerApi"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
