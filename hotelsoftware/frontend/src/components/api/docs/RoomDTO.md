ee6b167242adfce130037679f39641ae6ee3d68c
fabian_egartner <fabian.egartner@hotmail.com>
2022-01-23 14:17:31 +0100

    building project working

---
+++ hotelsoftware/frontend/src/components/api/docs/RoomDTO.md
@@ -0,0 +1,39 @@
+# OpenApiDefinition.RoomDTO
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**id** | **Number** |  | [optional] 
+**roomCategory** | **String** |  | [optional] 
+**roomNumber** | **Number** |  | [optional] 
+**roomStatus** | **String** |  | [optional] 
+**bookingId** | [**BookingId**](BookingId.md) |  | [optional] 
+
+
+
+## Enum: RoomCategoryEnum
+
+
+* `SINGLE` (value: `"SINGLE"`)
+
+* `DOUBLE` (value: `"DOUBLE"`)
+
+* `SUPERIOR` (value: `"SUPERIOR"`)
+
+
+
+
+
+## Enum: RoomStatusEnum
+
+
+* `FREE` (value: `"FREE"`)
+
+* `OCCUPIED` (value: `"OCCUPIED"`)
+
+* `CLEANING` (value: `"CLEANING"`)
+
+
+
+
--- hotelsoftware/src/main/java/at/fhv/hotelsoftware/view/BookingController.java
+++ hotelsoftware/src/main/java/at/fhv/hotelsoftware/view/BookingController.java
@@ -48,7 +48,10 @@ public class BookingController {
     private static final String CONFIRM_BOOKING = "/confirmBooking";
     private static final String SINGLE_PAGE_APPLICATION = "/spa";
     private static final String LIGHT = "/light";
+    private static final String INDEX = "index.html";
+
     private static final String ERROR_PAGE = "errorPage";
+
 
     @GetMapping(DASHBOARD_URL)
     public ModelAndView showDashboard(Model model) {
@@ -258,7 +261,7 @@ public class BookingController {
 
     @GetMapping (SINGLE_PAGE_APPLICATION)
     public ModelAndView spa(){
-        return new ModelAndView("index.html");
+        return new ModelAndView("redirect:" + INDEX);
     }
 
     @GetMapping (LIGHT)
